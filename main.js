d89d58131776910765d1b943b2ac62c5286ad264
albertpalacio <you@example.com>
2020-08-14 12:28:00 -0400

    aos added in

--- main.js
+++ main.js
@@ -9,5 +9,22 @@ $(document).ready(function() {
         $('.top-nav').removeClass('open');
     });
 
-    
+    $('nav a[href*="#').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000)
+    });
+
+    $('#up').on('click', function() {
+        $('html, body').animate({
+            scrollTop:0
+        }, 2000)
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    })
+
 });
