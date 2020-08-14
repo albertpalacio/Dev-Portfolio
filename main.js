0f5502bd34505ca40b4be8c270d150f0ad9c509d
albertpalacio <you@example.com>
2020-08-14 10:04:48 -0400

    js added to fix menu. menu works correctly now

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-$(document).ready(function () {
-    $('menu-toggler').on('click', function () {
+$(document).ready(function() {
+    $('.menu-toggler').on('click', function() {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
+    });
+
+    $('.nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
     });
 });
